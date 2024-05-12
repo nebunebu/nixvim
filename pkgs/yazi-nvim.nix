3183d0a738740de2b6e44aad1f0f0488dfd7f6bb
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-12 17:36:18 -0400

    add yazi-nvim.nix

---
+++ pkgs/yazi-nvim.nix
@@ -0,0 +1,15 @@
+{ pkgs
+, vimUtils
+, ...
+}:
+vimUtils.buildVimPlugin {
+  pname = "triptych.nvim";
+  version = "2023-02-10";
+  src = fetchFromGitHub {
+    owner = "mikavilpas";
+    repo = "yazi.nvim";
+    rev = "2cf605783e66523d1ac83cc65b2196193d81adcd";
+    hash = "sha256-3P+xyTmzoyVvzG8B2nOEiVuUbutRu9skCKROQN8e/0w=";
+  }
+  meta.homepage = "https://github.com/mikavilpas";
+}
