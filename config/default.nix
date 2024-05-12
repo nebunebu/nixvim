5a636854ec3688b3666375efa914c119633760f2
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-12 17:11:22 -0400

    update

--- config/default.nix
+++ config/default.nix
@@ -1,15 +1,38 @@
+{ pkgs, ...}:
 {
   imports = [
     ./core/autoCmd.nix
     # ./core/customFunctions.nix
     # ./core/highlight.nix
-    ./core/options.nix
+    # ./core/options.nix
     # ./plugins/default.nix
   ];
 
-  # globals = {
-  #   mapleader = " ";
-  # };
   enableMan = true;
-  clipboard.providers.wl-copy.enable = true;
+  # clipboard.providers.wl-copy.enable = true;
+  options = {
+    autochdir = true;
+    number = true;
+    relativenumber = true;
+    expandtab = true;
+    shiftwidth = 2;
+    tabstop = 2;
+    smarttab = true;
+    autoindent = true;
+    linebreak = true;
+    hidden = true;
+    cindent = true;
+    undofile = true;
+    swapfile = false;
+    ignorecase = true;
+    smartcase = true;
+    incsearch = true;
+    fillchars = { eob = " "; };
+  };
+  extraPlugins = [
+    {
+      plugin = pkgs.vimPlugins.rose-pine;
+      config = "colorscheme rose-pine";
+    }
+  ];
 }
