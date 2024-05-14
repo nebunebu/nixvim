3555a1fdba2b8cd5a1d7febf242600a4aae07729
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-14 18:47:14 -0400

    feat(alpha): set up alpha
    Refs: on_press.raw, errors

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ My standalone nixvim config.
 nix run github:nebunebu/nixvim
 ```
 
+## Resources and Credits
 
-## Resources
-
-- [siph/building_a_standalone_nixvim.md](https://gist.github.com/siph/288b7c6b5f68a1902d28aebc95fde4c5)
+- [siph/building_a_standalone_nixvim.md](https://gist.github.com/siph/288b7c6b5f68a1902d28aebc95fde4c5) for setting up standalone conf
+- [hbjydev/hvim](https://github.com/hbjydev/hvim/blob/3bc249f9a4489ba7150ced08c1d448dbbf5a737a/config/ui/dashboard/default.nix) for setting up alpha.
 - [nix-community/nixvim](https://github.com/nix-community/nixvim)
--- config/plugins/alpha.nix
+++ config/plugins/alpha.nix
@@ -1,115 +1,100 @@
-let
-  pad = {
-    type = "padding";
-    val = 2;
-  };
-in
+{ lib, ... }:
 {
   plugins.alpha = {
     enable = true;
     iconsEnabled = true;
     theme = null;
-    layout = [
-      pad
-      {
-        opts = {
-          hl = "Type";
-          position = "center";
+    layout =
+      let
+        width = 30;
+        mkPadding = size: { type = "padding"; val = size; };
+        mkHeading = title: {
+          type = "text";
+          val = title + lib.concatStrings (lib.replicate ((width + 3) - (builtins.stringLength title)) " ");
+          opts = {
+            hl = "String";
+            position = "center";
+          };
         };
-        type = "text";
-        val = [
-          " ###//    // "
-          " ####//   // "
-          " #####//  // "
-          " ## ###// // "
-          " ##  ###//// "
-          " ##   ###/// "
-          " ##    ###// "
-        ];
-      }
-      pad
-      {
-        opts = {
-          hl = "Keyword";
-          position = "center";
+        mkButton = title: icon: command: shortcut: {
+          type = "button";
+          val = icon + "  " + title;
+          # FIX: callback error
+          # on_press.raw = "function() vim.cmd[[${command}]] end";
+          opts = {
+            keymap = [
+              "n"
+              shortcut
+              "<CMD>${command}<CR>"
+              {
+                noremap = true;
+                silent = true;
+                nowait = true;
+              }
+            ];
+            shortcut = shortcut;
+            position = "center";
+            cursor = 0;
+            width = width;
+            align_shortcut = "right";
+            hl_shortcut = "Keyword";
+          };
         };
-        type = "text";
-        val = "Let scholastic sophisters entangle themselves in their own cobwebs";
-      }
-      pad
-      # {
-      #   type = "group";
-      #   val = [
-      #     {
-      #       on_press = {
-      #         __raw = "function() vim.cmd[[ene]] end";
-      #       };
-      #       opts = {
-      #         shortcut = "n";
-      #       };
-      #       type = "button";
-      #       val = "  New file";
-      #     }
-      #     {
-      #       on_press = {
-      #         __raw = "function() vim.cmd[[qa]] end";
-      #       };
-      #       opts = {
-      #         shortcut = "q";
-      #       };
-      #       type = "button";
-      #       val = " Quit Neovim";
-      #     }
-      #   ];
-      # }
-    ];
+      in
+      [
+        (mkPadding 2)
+        {
+          opts = {
+            hl = "Macro";
+            position = "center";
+          };
+          type = "text";
+          val = [
+            " ##//    // "
+            " ###//   // "
+            " ####//  // "
+            " ## ##// // "
+            " ##  ##//// "
+            " ##   ##/// "
+            " ##    ##// "
+          ];
+        }
+        (mkPadding 2)
+        (mkHeading "configs")
+        (mkButton "nix-config" "󱄅" "e $HOME/.nix-config/flake.nix" "c")
+        (mkButton "nixvim" "" "e $HOME/.nixvim/flake.nix" "v")
+        (mkPadding 1)
+        (mkHeading "writing")
+        (mkButton "wiki" "󰖬" "e $HOME/.wiki/index.md" "w")
+        (mkPadding 1)
+        (mkHeading "editor")
+        (mkButton "new file" "" "enew" "e")
+        (mkButton "exit" "󰩈" "qa" "q")
+        (mkPadding 2)
+        # TODO: buttons to add
+        # (mkButton "journal" "󱙼" "test" "j")
+        # (mkButton "blog" "" "test" "b")
+        # (mkButton "recent files" "󱙼" "test" "o")
+        # (mkButton "marks" "󱙼" "test" "m")
+        # (mkButton "MRU" "󱙼" "test" "r")
+        # (mkButton "/tmp" "󱙼" "test" "t")
+        # TODO: make quote single attrset
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = "Let scholastic sophisters entangle";
+        }
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = "themselves in their own cobwebs";
+        }
+      ];
   };
 }
-# {
-#   type = "group";
-#   val = [
-#     {
-#       type = "padding";
-#       val = 2;
-#     }
-#     {
-#       type = "group";
-#       val = {
-#         entries = [
-#           {
-#             command = "<CMD>ene <CR>";
-#             desc = "  New file";
-#             shortcut = "e";
-#           }
-#           {
-#             command = "<CMD>e $HOME/.wiki/index.md<CR>";
-#             desc = "  Wiki";
-#             shortcut = "w";
-#           }
-#           {
-#             command = "<CMD>e $HOME/.nix-config/flake.nix<CR>";
-#             desc = " nix-config";
-#             shortcut = "c";
-#           }
-#           {
-#             command = ":qa<CR>";
-#             desc = "󰩈 Quit Neovim";
-#             shortcut = "q";
-#           }
-#         ];
-#       };
-#     }
-#     {
-#       type = "padding";
-#       val = 2;
-#     }
-#     {
-#       opts = {
-#         hl = "Keyword";
-#         position = "center";
-#       };
-#       type = "text";
-#       val = "Let scholastic sophisters entangle themselves in their own cobwebs";
-#     }
-#   ];
-# };
