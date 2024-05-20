29fa024972c23e02fe57c887a46a26f8186d7a57
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-19 20:06:08 -0400

    fix(cmp): make cmp functional
    Refs: needs, work

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ nix run github:nebunebu/nixvim
 
 - [siph/building_a_standalone_nixvim.md](https://gist.github.com/siph/288b7c6b5f68a1902d28aebc95fde4c5) for setting up standalone conf
 - [hbjydev/hvim](https://github.com/hbjydev/hvim/blob/3bc249f9a4489ba7150ced08c1d448dbbf5a737a/config/ui/dashboard/default.nix) for setting up alpha.
+- [spector700/Akari](https://github.com/spector700/Akari/blob/main/config/completion/cmp.nix) for setting up nvim-cmp.
 - [nix-community/nixvim](https://github.com/nix-community/nixvim)
--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,54 +1,164 @@
 {
-  plugins.luasnip.enable = true;
-
-  plugins.cmp = {
-    enable = true;
-    settings = {
-      window = {
-        completion.border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-        documentation.border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-      };
-      snippet = {
-        expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
-      };
-    };
-    autoEnableSources = true;
-    cmdline = {
-      "/" = {
-        mapping = {
-          __raw = "cmp.mapping.preset.cmdline()";
-        };
-        sources = [{ name = "buffer"; }];
-      };
-      ":" = {
-        mapping = {
-          __raw = "cmp.mapping.preset.cmdline()";
-        };
+  plugins = {
+    cmp-nvim-lsp.enable = true;
+    cmp-emoji.enable = true;
+    cmp-buffer.enable = true;
+    cmp-path.enable = true;
+    cmp_luasnip.enable = true;
+    cmp = {
+      enable = true;
+      # Preselect first entry
+      settings = {
+        completion.completeopt = "menu,menuone,noinsert";
         sources = [
+          { name = "nvim_lsp"; } # lsp
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested.
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            keywordLength = 3;
+          }
           { name = "path"; }
           {
-            name = "cmdline";
-            option = {
-              ignore_cmds = [
-                "Man"
-                "!"
-              ];
-            };
+            name = "luasnip";
+            keywordLength = 3;
           }
         ];
-      };
-      settings = {
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "luasnip"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
+
+        window = {
+          completion.border = "rounded";
+          documentation.border = "rounded";
+        };
+        experimental.ghost_text = true;
+
+        mapping = {
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+          "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+        snippet = {
+          expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
+        };
+        # formatting = {
+        #   fields = [ "abbr" "kind" "menu" ];
+        #   format =
+        #     # lua
+        #     ''
+        #       function(_, item)
+        #         local icons = {
+        #           Namespace = "󰌗",
+        #           Text = "󰊄",
+        #           Method = "󰆧",
+        #           Function = "󰡱",
+        #           Constructor = "",
+        #           Field = "󰜢",
+        #           Variable = "󰀫",
+        #           Class = "󰠱",
+        #           Interface = "",
+        #           Module = "󰕳",
+        #           Property = "",
+        #           Unit = "󰑭",
+        #           Value = "󰎠",
+        #           Enum = "",
+        #           Keyword = "󰌋",
+        #           Snippet = "",
+        #           Color = "󰏘",
+        #           File = "󰈚",
+        #           Reference = "󰈇",
+        #           Folder = "󰉋",
+        #           EnumMember = "",
+        #           Constant = "󰏿",
+        #           Struct = "󰙅",
+        #           Event = "",
+        #           Operator = "󰆕",
+        #           TypeParameter = "",
+        #           Table = "",
+        #           Object = "󰅩",
+        #           Tag = "",
+        #           Array = "[]",
+        #           Boolean = "",
+        #           Number = "",
+        #           Null = "󰟢",
+        #           String = "󰉿",
+        #           Calendar = "",
+        #           Watch = "󰥔",
+        #           Package = "",
+        #           Copilot = "",
+        #           Codeium = "",
+        #           TabNine = "",
+        #         }
+        #
+        #         local icon = icons[item.kind] or ""
+        #         item.kind = string.format("%s %s", icon, item.kind or "")
+        #         return item
+        #       end
+        #     '';
+        # };
       };
     };
   };
 }
+# {
+#   plugins.luasnip.enable = true;
+#
+#   plugins.cmp = {
+#     enable = true;
+#     settings = {
+#       window = {
+#         completion.border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+#         documentation.border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+#       };
+#       snippet = {
+#         expand = ''
+#           function(args)
+#             require('luasnip').lsp_expand(args.body)
+#           end
+#         '';
+#       };
+#     };
+#     autoEnableSources = true;
+#     cmdline = {
+#       "/" = {
+#         mapping = {
+#           __raw = "cmp.mapping.preset.cmdline()";
+#         };
+#         sources = [{ name = "buffer"; }];
+#       };
+#       ":" = {
+#         mapping = {
+#           __raw = "cmp.mapping.preset.cmdline()";
+#         };
+#         sources = [
+#           { name = "path"; }
+#           {
+#             name = "cmdline";
+#             option = {
+#               ignore_cmds = [
+#                 "Man"
+#                 "!"
+#               ];
+#             };
+#           }
+#         ];
+#       };
+#       settings = {
+#         sources = [
+#           { name = "nvim_lsp"; }
+#           { name = "luasnip"; }
+#           { name = "path"; }
+#           { name = "buffer"; }
+#         ];
+#       };
+#     };
+#   };
+# }
